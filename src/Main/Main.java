c09e2772dadc087bbe97ce42cc2955675f904ef5
mlitcollege <martin.laadoga@itcollege.ee>
2017-01-04 17:53:01 +0200

    start method fix

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,10 +1,20 @@
 package Main;
 
-public class Main {
+import javafx.application.Application;
+import javafx.stage.Stage;
 
+public class Main extends Application {
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		launch(args);
 
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		int howManyShips = 10;
+		new GameBoard(howManyShips);
+		
 	}
 
 }
