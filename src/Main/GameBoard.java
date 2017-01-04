a7aba3a8e17288e762c43b7e8e7e112486251011
mlitcollege <martin.laadoga@itcollege.ee>
2017-01-04 18:17:27 +0200

    GameBoard class update, adding new methods

--- src/Main/GameBoard.java
+++ src/Main/GameBoard.java
@@ -1,5 +1,97 @@
 package Main;
 
+import javafx.application.Platform;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
+
 public class GameBoard {
+
+	GridPane gridPane = new GridPane();
+	int lengthInPixels = 900;
+	int lengthInShips = 7;
+	Navy navy;
+	Scene scene = new Scene(gridPane, lengthInPixels, lengthInPixels);
+
+	// Main -> GameBoard
+	
+	public GameBoard(int howManyShips) {
+		
+		Stage gameBoardStage = new Stage();
+		gameBoardStage.setScene(scene);
+		gameBoardStage.show();
+		
+		// making new ships
+		navy = new Navy(howManyShips, lengthInShips); 
+		
+		// show ships on GameBoard
+		refreshGameBoard();
+		
+		// saves mouse click
+		setClickEvent(); 
+	}
+
+
+	// saves mouseclick/shot
+	
+	private void setClickEvent() {
+
+		gridPane.setOnMouseClicked(event -> {
+
+			Rectangle square = (Rectangle) event.getTarget();
+			int y = GridPane.getRowIndex(square);
+			int x = GridPane.getColumnIndex(square);
+			
+			navy.shot(new int[] { x, y });
+			
+			if (navy.isGameOver())
+				gameOver();
+			else
+				refreshGameBoard();
+
+		});
+	}
+
+	private void refreshGameBoard() {
+		for (int i = 0; i < lengthInShips; i++) {
+			for (int j = 0; j < lengthInShips; j++) {
+				
+				// Navy object returns correct color.
+				
+				Color colorStatus = navy.getPosColor(i, j);
+				double shipLength = lengthInPixels / lengthInShips;
+				Rectangle square = new Rectangle(shipLength, shipLength, colorStatus);
+				square.setStroke(Color.CADETBLUE);
+				gridPane.add(square, i, j);
+			}
+		}
+	}
+	
+	
+	// if game is over
+	
+	private void gameOver() {
+
+		Label label = new Label("KÕIK LAEVAD PÕHJAS!");
+		label.setFont(new Font("Serif", 50));
+		VBox gameOverBox = new VBox();
+		Button exitButton = new Button("Välju Mängust");
+		Button startButton = new Button("Mängi uuesti");
+		exitButton.setMaxSize(150, 50);
+		startButton.setMaxSize(150, 50);
+		exitButton.setOnAction(e -> Platform.exit());
+		startButton.setOnAction(e -> {
+			System.out.println("mäng algas");
+		});
+		gameOverBox.getChildren().addAll(label, exitButton, startButton);
+		scene.setRoot(gameOverBox);
+
+	}
 
 }
